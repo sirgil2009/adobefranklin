a5cd8d6f6ed5f64f2811248b46ac27d00fdc53ba
Gilbert Pierre-Louis <gpl@adobe.com>
2023-04-14 11:07:33 -0700

    stack

--- blocks/stack/stack.js
+++ blocks/stack/stack.js
@@ -5,24 +5,21 @@ export default function decorate(block) {
 
     let zinIndex = 1;
   /* change to ul, li */
-  const ul = document.createElement('ul');
+  const mainDiv = document.createElement('div');
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-
-    [...li.children].forEach((div) => {
+    const div = document.createElement('div');
+    div.innerHTML = row.innerHTML;
     if (div.children.length === 1 && div.querySelector('picture')){
-        div.className = 'cards-card-image';
-        div.querySelector('picture').style.zIndex = zIndex;
+        div.className = 'stack-image';
+        div.querySelector('picture').style.zIndex =zinIndex;
         zIndex++;
         div.querySelector('picture').addEventListener('click', function(){
             zIndex--;});
-        }
-    else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+    }
+    else div.className = 'stack-epmty';
+    mainDiv.append(div);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  mainDiv.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
-  block.append(ul);
+  block.append(mainDiv);
 }
